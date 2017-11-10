f98244843c8d997907f58675d43a8a49dcba22e1
212603218 <eyup.aksu@ge.com>
2017-11-10 16:52:30 +0300

    second commit

--- src/Parser.java
+++ src/Parser.java
@@ -1,14 +1,21 @@
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
+import java.io.IOException;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Parser {
 
 
-    public static void main(String args[]) throws SQLException {
+    public static void main(String args[]) throws SQLException, IOException, ParseException {
+
+        java.util.Date temp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS").parse("2012-07-10 14:58:00.000000");
+        System.out.println(temp);
 
         DatabaseTransaction dbTransaction= new DatabaseTransaction();
         dbTransaction.createDb();
+        dbTransaction.loadLogs("");
 
     }
 
